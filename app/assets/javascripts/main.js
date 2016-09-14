17dc112051aec8166bd6d7e8859533fe5ebe2318
codecademy <ccuser@codecademy.com>
2016-09-14 13:50:18 -0400

    skill chart fixed

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -31,7 +31,7 @@ $('#nav').affix({
 	$(document).scroll(function(){
 		var top = $('#skills').height()-$(window).scrollTop();
 		console.log(top);
-		if(top<20){
+		if(top<50){
 			if(index==0){	
 			
 				$('.chart').easyPieChart({
